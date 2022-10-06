0941dacba6b5af091fba55e15832b54c8ad4c256
Febi Permata <76872752+18611069@users.noreply.github.com>
2022-10-06 23:27:00 +0700

    Create Filtering, Sorting, and Calculating Data.sql

---
+++ Filtering, Sorting, and Calculating Data.sql
@@ -0,0 +1,95 @@
+------------ FILTERING DATA ------------
+
+# WHERE Clause Operators #
+
+      SELECT column_name, column_name
+      FROM table_name
+      WHERE column_name operators value;
+      
+Note:
+operators value ( =, <>, >, <, <=, >=, BETWEEN, IS NULL)
+
+> Examples
+SELECT ProductName, UnitPrice, SupplierID
+FROM Products
+WHERE ProductName = 'Tofu';
+
+SELECT ProductName, UnitPrice, SupplierID
+FROM Products
+WHERE ProductName <> 'Anna Frozen';
+
+SELECT ProductName, UnitPrice, SupplierID
+FROM Products
+WHERE UnitPrice >= 75;
+
+SELECT ProductName, UnitPrice, SupplierID, UnitsInStock
+FROM Products
+WHERE UnitInStock BETWEEN 15 AND 90;
+
+SELECT ProductName, UnitPrice, SupplierID, UnitsInStock
+FROM Products
+WHERE ProductName IS NULL;
+
+
+
+# IN, OR, and NOT Clause Operators #
+
+> IN examples ( always use () ) < 
+
+SELECT ProductName, UnitPrice, SupplierID, UnitsInStock
+FROM Products
+WHERE SupplierID IN (9, 10, 11);
+
+
+> OR examples (will not  evaluate the second conditions in a WHERE if first condition is met) < 
+
+SELECT ProductName, UnitPrice, SupplierID, UnitsInStock
+FROM Products
+WHERE ProductName = 'Tofu' OR 'Soy';
+
+
+> OR with AND examples <
+
+SELECT ProductName, UnitPrice, SupplierID, UnitsInStock
+FROM Products
+WHERE (SupplierID = 9 OR SupplierID = 11)
+AND UnitPrice > 15;
+
+
+> NOT example <
+
+SELECT * FROM Employees
+WHERE NOT City='London' AND NOT City='Seattle';
+
+
+------------ SORTING DATA ------------
+
+SELECT something
+FROM database
+ORDER BY characteristic
+
+Note:
+ORDER BY 2,3 means 2nd column and 3rd column
+DESC descending
+ASC ascending
+
+
+
+------------ CALCULATING DATA ------------
+
+> Multiplication example <
+
+SELECT ProductID, UnitsOnOrder, UnitPrice,
+UnitsOnOrder * UnitPrice AS Total_Order_Cost
+FROM Products;
+
+
+> Combining Math Operation example <
+
+SELECT ProductID, Quantity, UnitPrice, Discount,
+(UnitPrice - Discount)/Quantity AS Total_Cost
+FROM OrderDetails;
+
+
+
+source: Coursera
