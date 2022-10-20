16819f526551ee99b1d207c41c24a85fdd922916
Febi Permata <76872752+18611069@users.noreply.github.com>
2022-10-20 21:30:42 +0700

    Create Subqueries in SQL.sql

---
+++ Subqueries in SQL.sql
@@ -0,0 +1,31 @@
+# subqueries is queries embedded into other queries (a query inside other query) #
+
+SELECT CustomerID, CompanyName, Region
+FROM Customers
+WHERE customerID in (SELECT CustomerID FROM Orders WHERE Freight > 100);
+
+# Note : SELECT only a single column (can't retrieve all columns (*)) #
+
+SELECT Costumer_name, Costumer_contact
+FROM Customers
+WHERE cust_id IN 
+    SELECT customer_id
+    FROM Orders
+    WHERE order_number IN (SELECT order_number
+       FROM OrderItems
+          WHERE prod_name = 'Toothbrush');
+          
+
+
+------------ SUBQUERIES FOR CALCULATION ------------
+
+SELECT COUNT (*) AS orders
+FROM Orders
+WHERE customer_id = '12345';
+
+SELECT customer_name, customer_state
+  (SELECT COUNT (*) AS orders
+  FROM Orders
+  WHERE Orders.customer_id = Customer.customer_id) AS orders
+FROM customers
+ORDER BY Customer_name;
