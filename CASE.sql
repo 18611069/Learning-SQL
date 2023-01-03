6ca97977913855231e2412c1c012e5b74f7019dc
Febi Permata <76872752+18611069@users.noreply.github.com>
2023-01-03 21:32:30 +0700

    Update CASE.sql

--- CASE.sql
+++ CASE.sql
@@ -29,3 +29,12 @@ WHEN bytes >= 500001 THEN 'large'
 ELSE 'other'
 END bytescategory
 FROM tracks;
+
+SELECT MONTH(order_date) AS order_month, sum(item_price) AS total_price, 
+CASE
+	WHEN sum(item_price) >= 30000000000 THEN 'Target Achieved'
+    WHEN sum(item_price) <= 25000000000 THEN 'Less Performed'
+    ELSE 'Follow Up'
+END as remark
+FROM sales_retail_2019
+GROUP BY MONTH(order_date);
