a5d1ea28a63c65303f5d767ec9a71a1d723de250
Febi Permata <76872752+18611069@users.noreply.github.com>
2023-01-03 21:40:15 +0700

    Update CASE.sql

--- CASE.sql
+++ CASE.sql
@@ -30,6 +30,7 @@ ELSE 'other'
 END bytescategory
 FROM tracks;
 
+
 SELECT MONTH(order_date) AS order_month, sum(item_price) AS total_price, 
 CASE
 	WHEN sum(item_price) >= 30000000000 THEN 'Target Achieved'
@@ -38,3 +39,17 @@ CASE
 END as remark
 FROM sales_retail_2019
 GROUP BY MONTH(order_date);
+
+
+## Rata - Rata total belanja per kode pelanggan.
+SELECT kode_pelanggan, avg(total) as avg_total 
+FROM tr_penjualan
+GROUP BY kode_pelanggan;
+## Selain itu,  jangan lupa untuk menambahkan kolom baru dengan nama ‘kategori’ yang mengkategorikan total/revenue ke dalam 3 kategori: High: > 300K; Medium: 100K - 300K; Low: <100K.
+SELECT kode_transaksi,kode_pelanggan,no_urut,kode_produk, nama_produk, qty, total,
+CASE  
+    WHEN total > 300000 THEN 'High'
+    WHEN total < 100000 THEN 'Low'   
+    ELSE 'Medium'  
+END as kategori 
+FROM tr_penjualan;
