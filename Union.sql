867520c8851b5afe1744593c0155c82d2c7d0307
Febi Permata <76872752+18611069@users.noreply.github.com>
2023-01-06 16:06:37 +0700

    Update Union.sql

--- Union.sql
+++ Union.sql
@@ -1,4 +1,4 @@
-# to combine the result-set of two or more SELECT statement #
+# to COMBINE the result-set of two or more SELECT statement #
 # each SELECT statement within UNION must have the same number of columns #
 # columns must have similar data types #
 # the columns in each SELECT statement must be in the same order #
