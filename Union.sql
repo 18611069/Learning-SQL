42d49fe248270478094b51af4a34d5a9537e1b27
Febi Permata <76872752+18611069@users.noreply.github.com>
2023-01-07 18:43:15 +0700

    Update Union.sql

--- Union.sql
+++ Union.sql
@@ -3,6 +3,14 @@
 # columns must have similar data types #
 # the columns in each SELECT statement must be in the same order #
 
+select kode_produk, nama_produk,harga
+from ms_produk_1
+where harga < 100000
+union
+select kode_produk, nama_produk,harga
+from ms_produk_2
+where harga < 50000
+
 select * from tabel_a
 union
 select * from tabel_b;
