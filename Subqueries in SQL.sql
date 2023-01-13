944301c0bae42a14298e97c861c157492becd714
Febi Permata <76872752+18611069@users.noreply.github.com>
2023-01-13 15:36:04 +0700

    Update Subqueries in SQL.sql

--- Subqueries in SQL.sql
+++ Subqueries in SQL.sql
@@ -66,3 +66,15 @@ AND customer_id IN (
     FROM data_retail
     WHERE product = 'Jaket');
 
+
+#hitung total transaksi setiap customer per masing-masing produk#
+SELECT a.Customer_ID, a.Transaksi_Sepatu, b.Transaksi_Jaket
+FROM (select customer_id, sum(count_transaction) transaksi_sepatu
+      from data_retail
+      where product = 'sepatu'
+      group by 1) A
+JOIN (select Customer_ID, sum(Count_Transaction) transaksi_jaket
+      from data_retail
+      where product = 'Jaket'
+      group by 1) B on a.Customer_ID = b.Customer_ID
+ORDER BY 2 DESC,3 DESC
